wimleers/homebridge-sma-home-manager

commit 5bcf295c8d0e9d8391b24c6cfddb30e5c3d7ede4

Fix a bug I did not anticipate in winter time: production exceeding consumption >10⨉!
```
 This plugin generated a warning from the characteristic Self-Sufficiency: characteristic was supplied illegal value: number 1123 exceeded maximum of 1000. See https://homebridge.io/w/JtMGR for more info.
```

index.js
@@ -698,8 +698,8 @@ SMAHomeManager.prototype = {
 		if (m.import > 0) {
 			return Math.min((m.production - m.export) / m.consumption * 100, 99);
 		}
-		// 0–10000%: production, no import.
-		return m.production / m.consumption * 100;
+		// 100–1000%: production, no import. (Limit to 1000%: 10⨉ consumption!)
+		return Math.max(m.production / m.consumption * 100, 10000);
 	},
 
 	// TRICKY: https://github.com/nodejs/node/issues/39377

commit 075b785d19d35a85859530e028e58cbeca3c6641

Fix incorrect self-sufficiency level when import=0 and production > consumption. Fixes regression introduced in 5bcf295.

index.js
@@ -699,7 +699,7 @@ SMAHomeManager.prototype = {
 			return Math.min((m.production - m.export) / m.consumption * 100, 99);
 		}
 		// 100–1000%: production, no import. (Limit to 1000%: 10⨉ consumption!)
-		return Math.max(m.production / m.consumption * 100, 10000);
+		return Math.min(m.production / m.consumption * 100, 10000);
 	},
 
 	// TRICKY: https://github.com/nodejs/node/issues/39377